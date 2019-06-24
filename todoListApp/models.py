1f3b9bbdffce07cbd39caf2e7b1feab709ac2483
AuriCA32 <auri.git.17@gmail.com>
2019-06-24 10:31:12 -0400

    Added To-do model

--- todoListApp/models.py
+++ todoListApp/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+class TodoList(models.Model):
+    PENDING = 1
+    COMPLETED = 0
+    STATUS_CHOICES = (
+		(PENDING, 'pending'),
+		(COMPLETED, 'completed'),
+	)
+    title = models.CharField(max_length=250) 
+    body = models.TextField(blank=True)
+    creation_date = models.DateField(default=timezone.now().strftime("%Y-%m-%d"))
+    status = models.PositiveSmallIntegerField(choices=STATUS_CHOICES, default=1)
+
+    def __str__(self):
+        return self.title
