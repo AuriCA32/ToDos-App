43062755bfa57413ba6c1d65f2fd1239f6a0eac1
AuriCA32 <auri.git.17@gmail.com>
2019-06-24 11:48:38 -0400

    Modified settings to properly use static files

--- miniappTodoList/settings.py
+++ miniappTodoList/settings.py
@@ -118,7 +118,9 @@ USE_L10N = True
 
 USE_TZ = True
 
-
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+]
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
