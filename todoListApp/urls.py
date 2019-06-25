b5dbc0897aba09120d9c602e91ffdbb494191cf3
AuriCA32 <auri.git.17@gmail.com>
2019-06-24 22:46:00 -0400

    Fixed urls for deleted menu view

--- todoListApp/urls.py
+++ todoListApp/urls.py
@@ -6,7 +6,6 @@ from . import views
 urlpatterns = [
     path('signup/', views.SignUp, name='signup'),
     path('login/', views.LogIn, name='login'),
-    path('menu/', views.menu, name='menu'),
     path('', views.index, name='index'),
     path('create/', views.createTask, name='create'),
     re_path(r'edit_*', views.editTask, name='edit'),
